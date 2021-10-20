8bbdee37569f5383c283a98e64d4e6eadaea13dc
coder-abhi <abhi.kamble.3987@gmail.com>
2021-10-20 22:49:34 +0530

    Header Image is set

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProfileImage from './MyImg.jpg'
+import './Header.css'
+function Header() {
+    return (
+        <div className="header">
+            <div className="circle-img">
+                    {/* <ProfileImage height='400' width='400'/> */}
+                    <img src={ProfileImage} className='profile-img'></img>
+                    {/* <h1 className="">Hi, I am Abhishek</h1>
+                    <h1 className="">Open Source Enthusist</h1> */}
+            </div>
+        </div>
+        )
+}
+
+export default Header
