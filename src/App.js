d2817a9d4f638c4fd8f87ccaf1f2e7ffc5daf837
coder-abhi <abhi.kamble.3987@gmail.com>
2021-10-22 15:37:24 +0530

    Background Image is added propery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Header';
+import Center from './Center';
 function App() {
   return (
     <div className="App">
     <Header />
+    <Center />
     </div>
   );
 }
---
+++ src/Center.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Center.css'
+function Center() {
+    return (
+        <div className="center">
+            <div className="center-slots"></div>
+            <div className="center-slots"></div>
+            <div className="center-slots"></div>
+            <div className="center-slots"></div>
+        </div>
+    )
+}
+
+export default Center
