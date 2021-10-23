e3ca8bc73a5dbfab77ac0746223f32cb52d1e957
coder-abhi <abhi.kamble.3987@gmail.com>
2021-10-23 11:00:21 +0530

    Progress bar is added.

--- src/Center.js
+++ src/Center.js
@@ -4,9 +4,39 @@ function Center() {
     return (
         <div className="center">
             <div className="center-slots">
-                <div className="center-slots-sub sub-slot-1"></div>
-                <div className="center-slots-sub sub-slot-2"></div>
+
+                <div className="center-slots-sub sub-slot-1">
+                    <div className='center-slots-sub-inner'>Hii</div>
+                </div>
+
+                <div className="center-slots-sub sub-slot-2">
+                    <div className='center-slots-sub-inner'>
+                        <div className='slot-heading'>Skills</div>
+                        <div className='skill-label'>
+                            <div className='slot-sub-head'>First</div>
+                            <div>32%</div>
+                        </div>
+                        <progress id="file" value="32" max="100" className='progress-bar' />
+                        <div className='skill-label'>
+                            <div className='slot-sub-head'>First</div>
+                            <div>32%</div>
+                        </div>
+                        <progress id="file" value="32" max="100" className='progress-bar' />
+                        <div className='skill-label'>
+                            <div className='slot-sub-head'>First</div>
+                            <div>32%</div>
+                        </div>
+                        <progress id="file" value="32" max="100" className='progress-bar' />
+                        <div className='skill-label'>
+                            <div className='slot-sub-head'>First</div>
+                            <div>32%</div>
+                        </div>
+                        <progress id="file" value="32" max="100" className='progress-bar' />
+                    </div>
+                </div>
+
             </div>
+
             <div className="center-slots">
                 <div className="center-slots-sub sub-slot-1"></div>
                 <div className="center-slots-sub sub-slot-2"></div>
