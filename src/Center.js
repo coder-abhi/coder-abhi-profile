a3c5ed0a6f89c2f0a3f31ac77dae37da2eff2fac
coder-abhi <abhi.kamble.3987@gmail.com>
2021-10-23 12:28:16 +0530

    Abut Me Slot Inserted

--- src/Center.js
+++ src/Center.js
@@ -6,7 +6,13 @@ function Center() {
             <div className="center-slots">
 
                 <div className="center-slots-sub sub-slot-1">
-                    <div className='center-slots-sub-inner'>Hii</div>
+                    <div className='center-slots-sub-inner'>
+                        <div className='slot-heading'>Abut Me</div>
+                        <div className='about-me'><br /> &emsp; &emsp; I always love to learn new things. Learning is like a hobby for me. Completed Diploma in Mechanical and Currently pursuing B.E. in Information technology.<br /> <br />
+                        &emsp; &emsp; Good grab of languages like C++ and Python and efficient in Data Structures and Algorithm. also Good performance in Competative Coding. <br /> <br />
+                        &emsp; &emsp; Most of the Project work is done in React, Javascript, Java, Python, C++, etc.
+                            Other than academics hobbies are flute, poetry, Calligraphy, Reading.</div>
+                    </div>
                 </div>
 
                 <div className="center-slots-sub sub-slot-2">
